1c21cf748bd7d414fe565d5a0ac6045637e160fd
Ricky Hendra Pratama <60565163+Ricky-hentama@users.noreply.github.com>
2020-10-29 11:16:15 +0700

    update 3.0
    portfolio added

--- assets/js/script.js
+++ assets/js/script.js
@@ -166,9 +166,9 @@ $(document).ready(function () {
       } 
   });
   // aos
-
    AOS.init(); 
-
+ 
+   
     /* Card Slider - Swiper */
 	var cardSlider = new Swiper('.card-slider', {
 		autoplay: {
@@ -181,6 +181,32 @@ $(document).ready(function () {
 			prevEl: '.swiper-button-next'
 		}
     });
+    
+  // portfolio
+   // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+   
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function() {
+      $('.venobox').venobox();
+    });
+  });
 
  
+ 
+
 });
